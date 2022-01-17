e8a0635bfa235fcad973c7b1dca379539e2a5cbb
浮烟 <rx6@qq.com>
2022-01-18 07:02:33 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="app">
+        <Header></Header>
+        <Breadcrumb
+            name="测试"
+            slug="test"
+            root="/test"
+            :publishEnable="true"
+            :adminEnable="true"
+            :feedbackEnable="true"
+            :crumbEnable="true"
+        >
+            <img slot="logo" svg-inline :src="getAppIcon('macro')" />
+        </Breadcrumb>
+        <LeftSidebar>left side</LeftSidebar>
+        <Main :withoutRight="false">
+            <div class="m-main">
+                <router-view />
+            </div>
+            <RightSidebar>right side</RightSidebar>
+            <Footer></Footer>
+        </Main>
+    </div>
+</template>
+
+<script>
+import {getAppIcon} from '@jx3box/jx3box-common/js/utils'
+export default {
+    name: "App",
+    props: [],
+    data: function () {
+        return {};
+    },
+    computed: {},
+    methods: {getAppIcon},
+    components: {},
+};
+</script>
+
+<style lang="less">
+@import "./assets/css/app.less";
+@media screen and (max-width: @phone) {
+    .c-crumb {
+        .none;
+    }
+}
+</style>
