55be5b956b15c82e7a0f4083f05ab21d19682f84
TYguigui <ghoghost@qq.com>
2022-01-23 10:41:44 +0800

    pet0122

--- src/pages/adventure/AdventureList.vue
+++ src/pages/adventure/AdventureList.vue
@@ -1,26 +1,51 @@
 <template>
-    <div class="v-adventure-list">
-        奇遇大全列表页
+  <div class="v-adventure-list">
+    <div class="listGuide flex">
+      <img src="../../assets/img/petbg.png" class="bg" />
+      <div class="guideTil flex">
+        <img src="../../assets/img/pettitle.png" />
+        <img src="../../assets/img/boxlogo.png" />
+      </div>
+      <div class="screen flex">
+        <div class="select flex">
+          <el-radio-group v-model="adventureClassNum">
+            <el-radio
+              v-for="item in adventureClass"
+              :key="item.id"
+              :label="item.id"
+              >{{ item.name }}</el-radio
+            >
+          </el-radio-group>
+        </div>
+        <div class="search flex">
+          <el-input placeholder="输入奇遇名字搜索" v-model="keyword" clearable>
+          </el-input>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import adventureClass from "../../assets/data/adventure_class.json";
 export default {
-   name : 'adventureList',
-   props:[],
-   components : {},
-   data : function(){
-       return {
-           
-       }
-   },
-   computed:{},
-   watch:{},
-   methods:{},
-   filters:{},
-   created:function(){},
-   mounted:function(){},
-}
+  name: "adventureList",
+  props: [],
+  components: {},
+  data: function () {
+    return {
+      adventureClass,
+      adventureClassNum: 0,
+      keyword: "",
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  filters: {},
+  created: function () {},
+  mounted: function () {},
+};
 </script>
 
 <style lang="less">
---
+++ src/service/post.js
@@ -0,0 +1,28 @@
+import { $cms } from "@jx3box/jx3box-common/js/https";
+
+function getMyPost(params) {
+    return $cms().get("/api/cms/posts/my", {
+        params: params,
+    });
+}
+
+function getPosts(params) {
+    let query = {
+        type: "json",
+    };
+    if (params) {
+        query = Object.assign(query, params);
+    }
+    return $cms().get("/api/cms/posts", {
+        params: query,
+    });
+}
+function getPost(id) {
+    return $cms().get(`/api/cms/post/${id}`);
+}
+
+function getMyPostCount() {
+    return $cms().get("/api/cms/posts/user/my/count");
+}
+
+export { getPosts, getPost, getMyPostCount, getMyPost };
