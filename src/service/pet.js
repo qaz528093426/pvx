b92bf0e27ba228cf3051c9fbce5a6c844f1473df
iRuxu <rx6@qq.com>
2022-01-25 23:49:12 +0800

    test:pet api

--- src/service/pet.js
+++ src/service/pet.js
@@ -1,32 +1,43 @@
-import { $node } from "@jx3box/jx3box-common/js/https";
-import {__iconPath} from "@jx3box/jx3box-common/data/jx3box.json"
-import axios from "axios"
+// import { $node } from "@jx3box/jx3box-common/js/https";
+import { __iconPath } from "@jx3box/jx3box-common/data/jx3box.json";
+import axios from "axios";
+
+// TODO:
+const $ = axios.create({
+    baseURL : 'http://localhost:7002/'
+})
+// const $ = $node()
+
 function getPets(params) {
-    return $node().get("/pets", {
+    return $.get("/pets", {
         params: params,
     });
 }
 
-function getPet(petid, client = 'std') {
-    return $node().get(`/pet/${petid}?client=${client}`);
+function getPet(petid, client = "std") {
+    return $.get(`/pet/${petid}`, {
+        params: {
+            client,
+        },
+    });
 }
 
 function getPetSkill(petid) {
-    return $node().get(`/pet/${petid}/skill`);
+    return $.get(`/pet/${petid}/skill`);
 }
 
-function getShopInfo({ item_type, item_id, client = 'std' }) {
-    return $node().get(`/shop`, {
+function getShopInfo({ item_type, item_id, client = "std" }) {
+    return $.get(`/shop`, {
         params: {
             client,
             itemType: item_type,
-            itemId: item_id
-        }
-    })
+            itemId: item_id,
+        },
+    });
 }
 
-function getPetLucky (){
-    return axios.get(__iconPath + "pvx/pet/output/pet_lucky.json")
+function getPetLucky() {
+    return axios.get(__iconPath + "pvx/pet/output/pet_lucky.json");
 }
 
-export { getPets, getPet, getPetSkill, getShopInfo , getPetLucky };
+export { getPets, getPet, getPetSkill, getShopInfo, getPetLucky };
