20981f20a812b0bd5514841415f6fe9db11f26d1
litthy <litthy@qq.com>
2022-01-26 17:45:58 +0800

    feat:修改样式和添加返回按钮

--- src/pages/trigger/TriggerSingle.vue
+++ src/pages/trigger/TriggerSingle.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="v-trigger-single">
+        <div class="m-trigger-navigation">
+            <el-button class="u-goback" size="medium" icon="el-icon-arrow-left" @click="goBack" plain>返回列表</el-button>
+            <div class="m-trigger-links">
+                <a class="u-link u-achievement" :href="getLink('cj', achieve_id)">
+                    <i class="el-icon-trophy"></i>
+                    成就信息
+                </a>
+            </div>
+        </div>
+
         <div class="m-details">
             <TriggerImgs :task="task" />
             <TriggerItem />
@@ -17,7 +27,7 @@ import TriggerGame from "@/components/trigger/game.vue";
 import Detail from "@/components/wiki/Detail.vue";
 import { getAdventureID, getAdventureTask, getSerendipityJson } from "@/service/adventure";
 import { __iconPath } from "@jx3box/jx3box-common/data/jx3box";
-
+import { getLink } from "@jx3box/jx3box-common/js/utils";
 export default {
     name: "triggerSingle",
     props: ["id", "type"],
@@ -35,19 +45,20 @@ export default {
     computed: {},
     watch: {},
     methods: {
+        getLink,
         getAdventure() {
-            getAdventureID(this.id).then((res) => {
+            getAdventureID(this.id).then(res => {
                 this.list = res.data;
                 this.task.name = res.data.szName;
             });
-            getAdventureTask(this.id).then((res) => {
+            getAdventureTask(this.id).then(res => {
                 let list = [];
-                res.data?.forEach((item) => {
+                res.data?.forEach(item => {
                     const key = item.szFramePath;
                     if (!list.includes(key)) list.push(key);
                 });
                 let arr = [];
-                list.forEach((e) => {
+                list.forEach(e => {
                     if (e) {
                         let k = e.replace(/\\/g, "/");
                         arr.push(this.imgNameTga(k));
@@ -55,10 +66,8 @@ export default {
                 });
                 this.task.list = arr;
             });
-            getSerendipityJson().then((res) => {
-                let id = res.data[this.id];
-
-                this.achieve_id = id;
+            getSerendipityJson().then(res => {
+                this.achieve_id = res.data[this.id];
             });
         },
 
@@ -66,11 +75,14 @@ export default {
             link = link.split(".tga");
             return link[0];
         },
+        goBack() {
+            this.$router.push({ name: "list" });
+        },
     },
     filters: {},
     created: function () {
         if (this.type == "adventure") this.getAdventure();
-        console.log(this.id, this.type, this.serendipityJson);
+        console.log(this.id, this.type);
     },
     mounted: function () {},
 };
