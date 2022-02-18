b4897d84e031429f508461772ae9ddba0c567152
yuanjintao <528093426@qq.com>
2022-02-18 17:16:20 +0800

    技艺助手初版

---
+++ src/components/cooking/panel/Panel.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="Panel">
+    <div class="m-Panel-content">
+      <div class="c-Panel-itemList">
+        <div
+          v-for="item in panelArr"
+          :key="item.id"
+          class="c-Panel-itemNav flex-center-between"
+        >
+          <div>
+            <div>物品图标</div>
+            <div>{{item.name}}</div>
+            <div>需要金钱</div>
+          </div>
+          <div>
+            <el-input-number
+              v-model="item.num"
+              :min="1"
+            ></el-input-number>
+            <el-button size="mini">
+              移出购物车
+            </el-button>
+          </div>
+        </div>
+      </div>
+      <div class="c-Panel-total">
+        总共：
+      </div>
+    </div>
+    <div class="m-Panel-content">
+      <div class="c-Panel-itemList">
+        <div class="c-Panel-itemNav flex-center-between">
+          <div>物品图标</div>
+          <div>物品名字</div>
+          <div>需要金钱</div>
+        </div>
+      </div>
+      <div class="c-Panel-total">
+        总共：
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Panel",
+  props: ["panelArr"],
+  data: function () {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+@import "~@/assets/css/cooking/index.less";
+</style>
