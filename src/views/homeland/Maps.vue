41fc74dec03921760b5d6c78b4c4495d9957c389
zixuan <litthy@qq.com>
2022-02-22 17:42:08 +0800

    maps && Tutorial 样式 #25

--- src/views/homeland/Maps.vue
+++ src/views/homeland/Maps.vue
@@ -1,34 +1,34 @@
 <template>
-    <div class="v-homeland-maps">
-        <h1 class="m-homeland-title">家园地图</h1>
+	<div class="v-homeland-maps">
+		<h1 class="m-homeland-title">家园地图</h1>
 
-        <!-- TODO:地图tabs -->
-        <div class="m-homeland-maps-tabs">
-            <el-tabs v-model="active">
-                <el-tab-pane v-for="(label, map_id) in data" :key="map_id" :label="label" :name="map_id">
-                    <div class="m-house-map">
-                        <div class="m-house-model">
-                            <div class="u-map">
-                                <img class="u-map-bg" :src="getMapImage(map_id)" />
-                            </div>
-                            <div class="u-house">
-                                <el-tooltip class="item" effect="light" placement="top" v-for="(house, i) in active_coords" :key="'house-' + i" popper-class="m-house-map-pop">
-                                    <span slot="content" class="u-house-desc">
-                                        {{ house.name }} <br />
-                                        面积：{{ house.area }} <br />
-                                        价格：<GamePrice :price="~~house.price * 10000" />
-                                    </span>
-                                    <span class="u-house-item" :class="'u-house-index-' + i" :style="buildStyle(house)">
-                                        <i :class="'u-house-icon u-house-area-' + house.area"></i>
-                                    </span>
-                                </el-tooltip>
-                            </div>
-                        </div>
-                    </div>
-                </el-tab-pane>
-            </el-tabs>
-        </div>
-    </div>
+		<!-- TODO:地图tabs -->
+		<div class="m-homeland-maps-tabs">
+			<el-tabs v-model="active">
+				<el-tab-pane v-for="(label, map_id) in data" :key="map_id" :label="label" :name="map_id">
+					<div class="m-house-map">
+						<div class="m-house-model">
+							<div class="u-map">
+								<img class="u-map-bg" :src="getMapImage(map_id)" />
+							</div>
+							<div class="u-house">
+								<el-tooltip class="item" effect="light" placement="top" v-for="(house, i) in active_coords" :key="'house-' + i" popper-class="m-house-map-pop">
+									<span slot="content" class="u-house-desc">
+										{{ house.name }} <br />
+										面积：{{ house.area }} <br />
+										价格：<GamePrice :price="~~house.price * 10000" />
+									</span>
+									<span class="u-house-item" :class="'u-house-index-' + i" :style="buildStyle(house)">
+										<i :class="'u-house-icon u-house-area-' + house.area"></i>
+									</span>
+								</el-tooltip>
+							</div>
+						</div>
+					</div>
+				</el-tab-pane>
+			</el-tabs>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -42,64 +42,87 @@ import GamePrice from "@jx3box/jx3box-common-ui/src/wiki/GamePrice.vue";
 const DEFAULT_MAP_ID = "455"; //默认地图
 
 export default {
-    name: "Maps",
-    props: [],
-    components: { GamePrice },
-    data: function () {
-        return {
-            active: DEFAULT_MAP_ID,
-            data: homeland_maps,
+	name: "Maps",
+	props: [],
+	components: { GamePrice },
+	data: function () {
+		return {
+			active: DEFAULT_MAP_ID,
+			data: homeland_maps,
 
-            coords: {},
-            active_coords: [],
-        };
-    },
-    computed: {},
-    watch: {
-        // 重载坐标数据
-        active: {
-            handler: function (map_id) {
-                this.loadCoords(map_id);
-            },
-        },
-    },
-    methods: {
-        // 显示家园地图图片
-        getMapImage: function (map_id) {
-            // return resolveImagePath(__ossRoot + "image/house/map/" + map_id + ".png")
-            return __imgPath + "image/house/map/" + map_id + ".png";
-        },
-        // 坐标计算
-        buildStyle: function (house) {
-            let x = (house.x / 1024) * 100 + "%";
-            let y = (house.y / 896) * 100 + "%";
-            return {
-                left: x,
-                top: y,
-            };
-        },
-        // 获取坐标数据
-        loadCoords: function (map_id) {
-            if (this.coords[map_id]?.length) {
-                this.active_coords = this.coords[map_id];
-            } else {
-                getHomelandCoord(map_id).then((res) => {
-                    this.coords[map_id] = res.data;
-                    this.active_coords = res.data;
-                });
-            }
-        },
-        // 初始化坐标仓库
-        init: function () {
-            Object.keys(homeland_maps).forEach((map_id) => {
-                this.coords[map_id] = [];
-            });
-        },
-    },
-    mounted: function () {
-        this.init();
-        this.loadCoords(DEFAULT_MAP_ID);
-    },
+			coords: {},
+			active_coords: [],
+
+			map_id: "",
+		};
+	},
+	computed: {
+		query: function () {
+			return this.$route.query.map_id;
+		},
+	},
+	watch: {
+		// 重载坐标数据
+		active: {
+			handler: function (map_id) {
+				this.loadCoords(map_id);
+
+				this.$router
+					.push({
+						query: {
+							map_id,
+						},
+					})
+					.catch((err) => err);
+			},
+		},
+		query: {
+			deep: true,
+			immediate: true,
+			handler: function (val) {
+				if (val) {
+					this.active = val;
+				}
+			},
+		},
+	},
+	methods: {
+		// 显示家园地图图片
+		getMapImage: function (map_id) {
+			// return resolveImagePath(__ossRoot + "image/house/map/" + map_id + ".png")
+			return __imgPath + "image/house/map/" + map_id + ".png";
+		},
+		// 坐标计算
+		buildStyle: function (house) {
+			let x = (house.x / 1024) * 100 + "%";
+			let y = (house.y / 896) * 100 + "%";
+			return {
+				left: x,
+				top: y,
+			};
+		},
+		// 获取坐标数据
+		loadCoords: function (map_id) {
+			if (this.coords[map_id]?.length) {
+				this.active_coords = this.coords[map_id];
+			} else {
+				getHomelandCoord(map_id).then((res) => {
+					this.coords[map_id] = res.data;
+					this.active_coords = res.data;
+				});
+			}
+		},
+		// 初始化坐标仓库
+		init: function () {
+			Object.keys(homeland_maps).forEach((map_id) => {
+				this.coords[map_id] = [];
+			});
+		},
+	},
+	mounted: function () {
+		this.init();
+		this.loadCoords(DEFAULT_MAP_ID);
+	},
 };
 </script>
 
--- src/views/homeland/Tutorial.vue
+++ src/views/homeland/Tutorial.vue
@@ -1,115 +1,101 @@
 <template>
-    <div class="v-homeland-tutorial">
-        <h1 class="m-homeland-title">家园指南</h1>
+	<div class="v-homeland-tutorial">
+		<h1 class="m-homeland-title">家园指南</h1>
 
-        <div class="m-homeland-info">
-            <!-- TODO:配一张图 -->
-            <div class="m-homeland-games">
-                <ul>
-                    <li v-for="(item, i) in game_data" :key="'game-' + i">
-                        <el-popover placement="top-start" width="200" trigger="hover" :content="showGameTip(item.szTip)" popper-class="m-homeland-games-pop" v-if="showGameTip(item.szTip)">
-                            <span slot="reference"
-                                ><i class="u-pic"><img :src="showGamePic(item.szPath)" /></i> <span class="u-name">{{ item.szName }}</span></span
-                            >
-                        </el-popover>
-                        <template v-else>
-                            <i class="u-pic"><img :src="showGamePic(item.szPath)" /></i> <span class="u-name">{{ item.szName }}</span>
-                        </template>
-                    </li>
-                </ul>
-            </div>
+		<div class="m-homeland-info">
+			<div class="m-homeland-games">
+				<div v-for="(item, i) in game_data" :key="'game-' + i">
+					<el-popover placement="top" width="200" trigger="hover" :content="showGameTip(item.szTip)" popper-class="m-homeland-games-pop" v-if="showGameTip(item.szTip)">
+						<div class="u-box" slot="reference">
+							<i class="u-pic"><img :src="showGamePic(item.nUnLockFrame)" /></i> <span class="u-name">{{ item.szName }}</span>
+						</div>
+					</el-popover>
+					<template v-else>
+						<div class="u-box">
+							<i class="u-pic"><img :src="showGamePic(item.nUnLockFrame)" /></i> <span class="u-name">{{ item.szName }}</span>
+						</div>
+					</template>
+				</div>
+			</div>
 
-            <!-- TODO:优化表格样式 -->
-            <div class="m-homeland-levels">
-                <table>
-                    <caption>
-                        家园升级表
-                    </caption>
-                    <thead>
-                        <th>等级</th>
-                        <th>观赏</th>
-                        <th>实用</th>
-                        <th>坚固</th>
-                        <th>风水</th>
-                        <th>趣味</th>
-                        <th>园宅币</th>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(item, i) in level_data" :key="'level' + i">
-                            <td>{{ item.Level }}</td>
-                            <td>{{ item.Attribute1 }}</td>
-                            <td>{{ item.Attribute2 }}</td>
-                            <td>{{ item.Attribute3 }}</td>
-                            <td>{{ item.Attribute4 }}</td>
-                            <td>{{ item.Attribute5 }}</td>
-                            <td>{{ item.Currency }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+			<div class="m-homeland-levels">
+				<el-table :data="level_data" stripe>
+					<el-table-column align="center">
+						<template slot="header">
+							<h2 class="u-header"><i class="el-icon-s-home"></i> 家园升级需求</h2>
+						</template>
+						<el-table-column prop="Level" label="等级" align="center" width="100px"> </el-table-column>
+						<el-table-column prop="Attribute1" label="观赏" align="center"> </el-table-column>
+						<el-table-column prop="Attribute2" label="实用" align="center"> </el-table-column>
+						<el-table-column prop="Attribute3" label="坚固" align="center"> </el-table-column>
+						<el-table-column prop="Attribute4" label="风水" align="center"> </el-table-column>
+						<el-table-column prop="Attribute5" label="趣味" align="center"> </el-table-column>
+						<el-table-column prop="Currency" label="园宅币" align="center"> </el-table-column>
+					</el-table-column>
+				</el-table>
+			</div>
 
-            <!-- TODO:家园作品推荐 -->
-            <div class="m-homeland-posts" v-if="posts && posts.length">
-                <h2 class="m-homeland-subtitle">家园攻略</h2>
-                <ul>
-                    <li v-for="(item,i) in posts" :key="'post-' + i">
-                        {{item}}
-                    </li>
-                </ul>
-            </div>
-
-        </div>
-
-    </div>
+			<div class="m-homeland-posts" v-if="posts && posts.length">
+				<h2 class="u-header"><i class="el-icon-star-on"></i> 家园攻略</h2>
+				<div class="u-box">
+					<a :href="item.link" target="_blank" class="u-item" v-for="item in 100" :key="item">{{ item }}</a>
+					<!-- <a :href="item.link" target="_blank" class="u-item" v-for="(item, i) in posts" :key="i">{{ item.label }}</a> -->
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import { getHomelandLevelUp, getHomelandGame } from "@/service/homeland.js";
-import {getMenu} from '@jx3box/jx3box-common/js/api_misc'
+import { getMenu } from "@jx3box/jx3box-common/js/api_misc";
+import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 export default {
-    name: "Tutorial",
-    props: [],
-    components: {},
-    data: function () {
-        return {
-            level_data: [],
-            game_data: [],
+	name: "Tutorial",
+	props: [],
+	components: {},
+	data: function () {
+		return {
+			level_data: [],
+			game_data: [],
 
-            posts : []
-        };
-    },
-    computed: {},
-    watch: {},
-    methods: {
-        // 加载基本信息
-        loadInfo: function () {
-            getHomelandLevelUp().then((res) => {
-                this.level_data = res?.data;
-            });
-            getHomelandGame().then((res) => {
-                this.game_data = res?.data;
-            });
-        },
-        // 加载推荐作品
-        loadData : function (){
-            getMenu('homeland_rec').then((data) => {
-                this.posts = data || []
-            })  
-        },
+			posts: [],
+		};
+	},
+	computed: {},
+	watch: {},
+	methods: {
+		// 加载基本信息
+		loadInfo: function () {
+			getHomelandLevelUp().then((res) => {
+				this.level_data = res?.data;
+			});
+			getHomelandGame().then((res) => {
+				this.game_data = res?.data;
+			});
+		},
+		// 加载推荐作品
+		loadData: function () {
+			getMenu("homeland_rec").then((data) => {
+				this.posts = data || [];
+			});
+		},
 
-        // 显示日常图标
-        showGamePic: function () {},
+		// 显示日常图标
+		showGamePic: function (index) {
+			return __imgPath + "image/game/homeland/seasonfurniture_" + index + ".png";
+		},
 
-        // 显示活动介绍
-        showGameTip : function (str){
-            let output = str?.split('\\n')
-            return output.length > 0 ? output[1] : ''
-        }
-    },
-    mounted: function () {
-        this.loadInfo();
-        this.loadData()
-    },
+		// 显示活动介绍
+		showGameTip: function (str) {
+			let output = str?.split("\\n");
+			return output.length > 0 ? output[1] : "";
+		},
+	},
+	mounted: function () {
+		this.loadInfo();
+		this.loadData();
+	},
 };
 </script>
 <style lang="less">
