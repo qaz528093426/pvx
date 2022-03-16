291d32de60d723a18d1708e6890a96b87a54a3bf
zixuan <litthy@qq.com>
2022-03-16 16:55:20 +0800

    fix:中间区域优化 #38

--- src/components/manufacture/Recipe.vue
+++ src/components/manufacture/Recipe.vue
@@ -1,16 +1,27 @@
 <template>
 	<div class="m-manufacture-recipe" v-if="item">
 		<!-- 配方信息展示 -->
-		<div class="u-img">
-			<div class="u-border" :style="{ backgroundImage: item_border(item.Quality), opacity: item.Quality == 5 ? 0.9 : 1 }"></div>
-			<img :src="iconLink(item.IconID)" :alt="item.Name" />
-		</div>
+
+		<el-popover popper-class="u-icon-popper" placement="right" :visible-arrow="false"  trigger="hover">
+			<Item :item_id="item_type_id" />
+			<div class="u-img" slot="reference">
+				<div class="u-border" :style="{ backgroundImage: item_border(item.Quality), opacity: item.Quality == 5 ? 0.9 : 1 }"></div>
+				<img :src="iconLink(item.IconID)" :alt="item.Name" />
+			</div>
+		</el-popover>
 
 		<span class="u-name" :class="`u-quality--${item.Quality}`">{{ item.Name }}</span>
+		<div class="u-price u-interval">[{{ server }}] 昨日平均价格:<GamePrice v-if="item_price" class="u-price-num" :price="item_price" /><span class="u-null" v-else>暂无数据</span></div>
 		<div class="u-info u-interval">
-			<span>需求等级: <b>{{ item.nLevel || "未知" }}</b></span>
-			<span>消耗精力: <b>{{ item.CostVigor }}</b></span>
-			<span>经验值: <b>{{ item.Exp }}</b></span>
+			<span
+				>需求等级: <b>{{ item.nLevel || "未知" }}</b></span
+			>
+			<span
+				>消耗精力: <b>{{ item.CostVigor }}</b></span
+			>
+			<span
+				>经验值: <b>{{ item.Exp }}</b></span
+			>
 		</div>
 		<template v-if="item.szTip">
 			<span class="u-desc" v-for="text in textFilter(item.szTip)" :key="text">{{ text }}</span>
@@ -18,16 +29,20 @@
 		<div class="u-children" v-if="child && child.length">
 			<el-divider content-position="left">合成所需材料</el-divider>
 			<div class="u-child u-interval" v-for="(el, index) in child" :key="index">
-				<div class="u-img">
-					<div class="u-border" :style="{ backgroundImage: item_border(el.Quality), opacity: el.Quality == 5 ? 0.9 : 1 }"></div>
-					<img :src="iconLink(el.item_info.IconID)" :alt="el.Name" srcset="" />
-				</div>
+				<el-popover popper-class="u-icon-popper" placement="right" :visible-arrow="false" width="auto" trigger="hover">
+					<Item :item_id="el.price_id" />
+					<div class="u-img" slot="reference">
+						<div class="u-border" :style="{ backgroundImage: item_border(el.Quality), opacity: el.Quality == 5 ? 0.9 : 1 }"></div>
+						<img :src="iconLink(el.item_info.IconID)" :alt="item.Name" />
+					</div>
+				</el-popover>
+
 				<div class="u-info">
 					<div class="u-name u-interval">
 						<span :class="`u-quality--${el.Quality}`"> {{ el.Name }}</span>
 						<span class="u-num">数量：x {{ el.count }}</span>
 					</div>
-					<div class="u-price">[{{server}}] 昨日平均价格:<GamePrice class="u-price-num" :price="el.Price" /></div>
+					<div class="u-price">[{{ server }}] 昨日平均价格:<GamePrice class="u-price-num" :price="el.Price" /></div>
 					<!-- <span class="u-interval">
 						<span class="u-desc" v-for="text in textFilter(el.item_info.Desc)" :key="text">{{ text }}</span>
 					</span> -->
@@ -41,6 +56,7 @@ import { getManufactureItem, getOther, getItemsPrice, getAuction } from "@/servi
 import { iconLink } from "@jx3box/jx3box-common/js/utils.js";
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 import GamePrice from "@jx3box/jx3box-common-ui/src/wiki/GamePrice.vue";
+import Item from "@jx3box/jx3box-editor/src/Item.vue";
 export default {
 	name: "Recipe",
 	props: ["id", "craft", "server"],
@@ -51,8 +67,19 @@ export default {
 			price: [],
 		};
 	},
-	components: { GamePrice },
-	computed: {},
+	components: { GamePrice, Item },
+	computed: {
+		item_price() {
+			let num = 0;
+			this.item.child.forEach((el) => {
+				if (el.id == this.item.CreateItemIndex1) num = el.Price;
+			});
+			return num;
+		},
+		item_type_id() {
+			return this.item.CreateItemType1 + "_" + this.item.CreateItemIndex1;
+		},
+	},
 	watch: {
 		id: {
 			deep: true,
@@ -123,16 +150,14 @@ export default {
 		// 获取价格
 		getItemPrice() {
 			if (this.item.child.length) {
-				let arr = [...this.item.child, { id: this.item.ID, price_id: this.item.CreateItemType1 + "_" + this.item.CreateItemIndex1 }];
+				let arr = [...this.item.child, { id: this.item.CreateItemIndex1, price_id: this.item_type_id }];
 				let _ids = arr.map((item) => item.id);
 				getItemsPrice({ ids: _ids.join(), client: this.$store.state.client }).then((res) => {
 					let _list = res.data;
 					let _arr = [];
 					let _child = [];
 					if (!_list.length) {
-						arr.forEach((item) => {
-							_arr.push(item.price_id);
-						});
+						_arr = arr.map((item) => item.price_id);
 					} else {
 						arr.forEach((item) => {
 							if (!_list.some((el) => el.ItemIndex == item.id)) {
@@ -161,7 +186,16 @@ export default {
 									});
 								}
 							}
-							this.item.child = arr;
+							this.child = this.child.map((item) => {
+								arr.forEach((el) => {
+									if (el.id == item.ID) {
+										item.Price = el.Price;
+										item.price_id = el.price_id;
+									}
+								});
+								return item;
+							});
+							this.item.child = _child;
 						});
 					}
 				});
