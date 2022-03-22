6637222a5251537709c8080401e00d08cc221400
iRuxu <rx6@qq.com>
2022-03-22 10:48:49 +0800

    fix:share

--- src/views/share/Share.vue
+++ src/views/share/Share.vue
@@ -1,8 +1,14 @@
 <template>
     <div id="app">
         <Header></Header>
-        <Breadcrumb name="捏脸妆容" slug="share" root="/share" :publishEnable="true" :adminEnable="true" :feedbackEnable="true" :crumbEnable="true">
-            <img slot="logo" svg-inline :src="getAppIcon('share')" />
+        <Breadcrumb
+            name="捏脸妆容"
+            slug="share"
+            root="/share"
+            :publishEnable="true"
+            :feedbackEnable="true"
+            :crumbEnable="true"
+        >
         </Breadcrumb>
         <LeftSidebar><Nav></Nav></LeftSidebar>
         <Main :withoutRight="true">
@@ -18,7 +24,6 @@
 
 <script>
 import Nav from "@/components/Nav.vue";
-import { getAppIcon } from "@jx3box/jx3box-common/js/utils";
 export default {
     name: "App",
     props: [],
@@ -26,11 +31,7 @@ export default {
         return {};
     },
     computed: {},
-    methods: { getAppIcon },
     components: { Nav },
-    created() {
-        // sessionStorage.setItem("jx3_ShareId", this.id);
-    },
 };
 </script>
 
