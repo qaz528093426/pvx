07e0d251e0a7d52a9fac0c8d3201beb68b6fb34a
X3ZvaWQ <x3zvawq@gmail.com>
2022-04-30 01:00:43 +0800

    feat: pet map

--- src/views/pet/PetSingle.vue
+++ src/views/pet/PetSingle.vue
@@ -72,7 +72,14 @@
             <!-- 羁绊信息 -->
             <petFetters :info="item" v-for="item in medalList" :key="item.ID" />
         </div>
-
+        <!-- 宠物地图 -->
+        <div class="m-pet-map" v-if="mapExist">
+            <div class="u-header"><img class="u-icon" svg-inline src="../../assets/img/achievement.svg" /> <span class="u-txt">宠物地图</span></div>
+            <!-- 地图组件 -->
+            <div class="u-warpper">
+                <pet-map :petId="parseInt(id)" />
+            </div>
+        </div>
         <!-- 宠物攻略 -->
         <div class="m-pet-wiki">
             <Wiki source_type="item" :source_id="item_id" :type="type" :id="id" title="宠物攻略" :source_title="title"></Wiki>
@@ -94,7 +101,9 @@ import petSource from "@/assets/data/pet_source.json";
 import { iconLink, getLink } from "@jx3box/jx3box-common/js/utils";
 import Comment from "@jx3box/jx3box-comment-ui/src/Comment.vue";
 import { postStat } from "@jx3box/jx3box-common/js/stat.js";
-import dayjs from 'dayjs'
+import dayjs from 'dayjs';
+import PetMap from 'petmap/src/components/PetMap.vue';
+import PetPOIs from 'petmap/src/assets/data/PetPOIs.json';
 
 export default {
     name: "PetSingle",
@@ -104,6 +113,7 @@ export default {
         petFetters,
         Wiki,
         Comment,
+        PetMap
     },
     data: function () {
         return {
@@ -137,6 +147,9 @@ export default {
                 client: this.client,
             };
         },
+        mapExist: function () {
+            return PetPOIs[`${this.id}`] != undefined;
+        }
     },
     watch: {
         id() {
@@ -293,4 +306,5 @@ export default {
 
 <style lang="less">
 @import "~@/assets/css/pet/single.less";
+@import "~@/assets/css/pet/map.less";
 </style>
